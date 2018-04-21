84694072175407ffcbffb45121612cbfd8aee508
Adrien HARNAY <adrien@harnay.me>
2018-04-21 16:23:32 +0200

    use resizeMode on image instead of negative margin

--- src/screens/details-screen/DetailsScreen.js
+++ src/screens/details-screen/DetailsScreen.js
@@ -46,8 +46,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: '100%',
-    height: 350,
-    marginTop: -50,
-    marginBottom: -100,
+    height: 250,
+    resizeMode: 'cover',
   },
 });
