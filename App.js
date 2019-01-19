4e8d335e8143395f1d3a04ccdeddcb94eefe97a8
Adrien HARNAY <adrien@harnay.me>
2019-01-19 11:58:11 +0100

    upgrade dependencies

--- App.js
+++ App.js
@@ -1,3 +1,57 @@
-import App from './src/App';
+import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { AsyncStorage, SafeAreaView, StyleSheet } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import CocktailsScreen from './src/screens/cocktails-screen/CocktailsScreen';
+import DetailsScreen from './src/screens/details-screen/DetailsScreen';
+
+const cache = new InMemoryCache();
+
+persistCache({
+  cache,
+  storage: AsyncStorage,
+});
+
+const client = new ApolloClient({
+  uri: 'https://demo-apollo-server.herokuapp.com/graphql',
+  cache,
+});
+
+const RootStack = createAppContainer(
+  createStackNavigator(
+    {
+      Home: CocktailsScreen,
+      Details: DetailsScreen,
+    },
+    {
+      initialRouteName: 'Home',
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#ffff',
+        },
+        headerBackTitle: null,
+      },
+    },
+  ),
+);
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <SafeAreaView style={styles.safeArea}>
+      <RootStack />
+    </SafeAreaView>
+  </ApolloProvider>
+);
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 
 export default App;
